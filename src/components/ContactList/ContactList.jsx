bddca5ce3554064714289fbdd695e439736eb750
Zhenya09 <klevets00@gmail.com>
2023-05-05 22:05:41 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 const ContactList = ({ contacts, delContact }) => {
-  const handleDelete = id => {
-    delContact(id);
-  };
+  // const handleDelete = id => {
+  //   delContact(id);
+  // };
 
   return (
     <ul className={css.list}>
@@ -12,7 +12,8 @@ const ContactList = ({ contacts, delContact }) => {
         return (
           <li className={css.item} key={contact.id}>
             {contact.name}: {contact.number}
-            <button onClick={() => handleDelete(contact.id)}>Delete</button>
+            {/* <button onClick={() => handleDelete(contact.id)}>Delete</button> */}
+             <button onClick={() =>  delContact(contact.id)}>Delete</button>
           </li>
         );
       })}
@@ -23,7 +24,8 @@ const ContactList = ({ contacts, delContact }) => {
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      // id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
     })
